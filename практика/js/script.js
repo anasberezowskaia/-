74f3308495570bf1fbf32048d63253d17c7f21ee
anasberezowskaia <113972731+anasberezowskaia@users.noreply.github.com>
2023-03-03 00:39:01 +0700

    Update script.js

--- практика/js/script.js
+++ практика/js/script.js
@@ -3,7 +3,7 @@ function myfunc() {
     var b1, b2, b3, b4, b5, b6, b7, b8, b9;
     b1 = document.getElementById("b1").value;
     b2 = document.getElementById("b2").value;
-    b3 = document.getElementById("b33").value;
+    b3 = document.getElementById("b3").value;
     b4 = document.getElementById("b4").value;
     b5 = document.getElementById("b5").value;
     b6 = document.getElementById("b6").value;
